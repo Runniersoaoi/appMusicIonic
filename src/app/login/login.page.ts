11cf75aeb4614be33391cfd50943fff98eaacfa4
MatiasAquinoC <71706694@continental.edu.pe>
2024-07-16 19:18:05 -0500

    feat: add reactive form and css utilites

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        '',
+        Validators.compose([
+          Validators.required,
+          // Validators.email,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9_.]+$'),
+        ])
+      ),
+      password: new FormControl(
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)])
+      ),
+    });
+  }
+
+  ngOnInit() {}
+  loginUser(dataLogin: any) {
+    console.log(dataLogin);
+  }
+}
